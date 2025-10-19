c427ec38be715b1540c1876b4e5c8d4dd8c6caf3
markjasperrojas <markjasperrojas1@gmail.com>
2025-10-19 10:32:02 +0800

    create index.html and scripts.js files, add getComputerChoice function

---
+++ script.js
@@ -0,0 +1,11 @@
+function getComputerChoice() {
+  const computerChoice = Math.floor(Math.random() * 3);
+
+  if (computerChoice === 0) {
+    return "rock";
+  } else if (computerChoice === 1) {
+    return "paper";
+  } else {
+    return "scissors";
+  }
+}
