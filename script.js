aaa300eb4d014b7d5e2c93bc233c460e32310f60
markjasperrojas <markjasperrojas1@gmail.com>
2026-01-03 18:23:29 +0800

    Add a winner function, modify some wordings, and finish the game logic

--- script.js
+++ script.js
@@ -2,6 +2,8 @@ const buttons = document.querySelectorAll("button");
 const playerScoreDisplay = document.querySelector("#player-score");
 const computerScoreDisplay = document.querySelector("#computer-score");
 const resultDisplay = document.querySelector("#result");
+const playAgain = document.querySelector("#play-again");
+const playAgainBtn = document.createElement("button");
 
 let playerScore = 0;
 let computerScore = 0;
@@ -28,19 +30,20 @@ function getComputerChoice() {
 
 function playRound(playerChoice, computerChoice) {
   if (playerChoice === computerChoice) {
-    resultDisplay.textContent = `Result: Tie! Both chose ${playerChoice}`;
-    return;
+    resultDisplay.textContent = `Result: Tie, both chose ${playerChoice}`;
   } else if (beats(playerChoice, computerChoice)) {
     playerScore++;
     playerScoreDisplay.textContent = `Player score: ${playerScore}`;
-    resultDisplay.textContent = `Result: You win! ${playerChoice} beats ${computerChoice}`;
-    return;
+    resultDisplay.textContent = `Result: You win, ${playerChoice} beats ${computerChoice}`;
   } else {
     computerScore++;
     computerScoreDisplay.textContent = `Computer score: ${computerScore}`;
-    resultDisplay.textContent = `Result: You lose! ${computerChoice} beats ${playerChoice}`;
-    return;
+    resultDisplay.textContent = `Result: You lose, ${computerChoice} beats ${playerChoice}`;
   }
+
+  winner();
+
+  return;
 }
 
 function beats(a, b) {
@@ -49,4 +52,24 @@ function beats(a, b) {
     (a === "paper" && b === "rock") ||
     (a === "scissors" && b === "paper")
   );
+}
+
+function winner() {
+  if (playerScore === 5 || computerScore === 5) {
+    playAgainBtn.textContent = "Play Again";
+
+    if (playerScore === 5) {
+      resultDisplay.textContent = "Congratulations! You won.";
+    } else {
+      resultDisplay.textContent = "Game Over!";
+    }
+
+    playAgainBtn.addEventListener("click", () => {
+      location.reload();
+    });
+
+    playAgain.appendChild(playAgainBtn);
+  }
+
+  return;
 }
